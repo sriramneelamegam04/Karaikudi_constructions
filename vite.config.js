4a050b405c06159082d3edceb6df8bd2909cba2f
Ranjani S <ranjanismarketing@gmail.com>
2026-09-19 20:16:52 +0530

    Initial project

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: '/Karaikudi_constructions/',
+
   server: {
     port: 5173,
     host: true
